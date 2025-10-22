8dc12ccd8820ebc0c9f140bf1963ae6dcd60c947
martinapm <martinapm99620@gmail.com>
2025-10-22 16:13:49 +0200

    numeros y notas

---
+++ UNIDAD 02/nota_alumno.py
@@ -0,0 +1,9 @@
+
+nota1 = float(input("Introduce la primera nota: "))
+nota2 = float(input("Introduce la segunda nota: "))
+nota3 = float(input("Introduce la tercera nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"Las notas introducidas son: {nota1}, {nota2}, {nota3}")
+print(f"La nota media es: {media:.2f}")
