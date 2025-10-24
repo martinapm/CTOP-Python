f9bdedc5f51d4e8914645d032fd6accfdd407114
martinapm <martinapm99620@gmail.com>
2025-10-24 19:04:26 +0200

    .

--- UNIDAD 02/try-except__2.py
+++ UNIDAD 02/try-except__2.py
@@ -3,12 +3,11 @@ import sys
 lista = [1, 2, 3, 4]
 
 try:
-    for n in range(5):
-        print(lista[n])
+    for n in lista:
+        print(n)
 except IndexError as e:
     print("Error :", e)
 else:
     print("Finalizó sin errores.")
 finally:
     sys.exit()
-    
