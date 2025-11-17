7cc9f12f67f19afde21b68b0ac13a230b971eea0
martinapm <martinapm99620@gmail.com>
2025-11-17 17:47:24 +0100

    UNIDAD 03

---
+++ UNIDAD 03/ejemplo_debug.py
@@ -0,0 +1,14 @@
+
+def promedio(numeros):
+    total = 0
+    for n in numeros:
+     total += n
+ # ERROR: usamos len(numeros)-1 en lugar de len(numeros)
+     return total / (len(numeros)-1)
+
+def main():
+ nums = [2, 4, 6]
+ print('Lista:', nums)
+ print('Promedio:', promedio(nums))
+if __name__ == '__main__':
+ main
